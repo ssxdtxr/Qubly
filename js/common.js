18be17d6fa80e61ea5d823c6f4cd02773349bea9
ssxdtxr <tereskinegor68@gmail.com>
2023-02-17 09:56:17 +0300

    release site

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(function () {
+  new WOW().init();
+
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+
+    if ($(this).hasClass("is-active")) {
+      $(".mnu_top").slideDown(300);
+    } else {
+      $(".mnu_top").slideUp(300);
+    }
+  });
+
+  // reviews tabs
+  $(".nav_rev > div").click(function () {
+    const revID = $(this).data("revid");
+
+    $(".nav_rev > div").not(this).removeClass("active");
+    $("this").addClass("active");
+
+    $(".item_rev").not(revID).removeClass("active");
+    $(revID).addClass("active");
+  });
+
+  // reviews tabs
+});
